61251823084c71fbfca16752dbbd513d5cbd1725
idan054 <idanbit80@gmai.com>
2021-04-27 00:38:08 +0300

    new main

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-# V3.0 im now on git checkout 8f6ef072925c9257078f1e34e32525b85ca2171f
 
-# V2.0 Everything here still
-
-# GUI push
-# GUI commit
-
-# V 1.0 Nothing here... Just me.
+ Im the new main bich
